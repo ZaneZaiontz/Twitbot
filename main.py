d7b73c1e7f04dac4b131e6dc1ddca664a81d7778
Zane Zaiontz <zanezaiontz@live.com>
2022-07-24 18:30:26 -0500

    Inital Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os, tweepy
+from csv import reader
+
+# 
+'''
+# TODO:
+#   Create dev account and setup tweepy
+#   -   Gather data over current trending tweets from the last 24 hours
+#   -   Store data in CSV / prepare
+#   -   
+#   -
+'''
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
