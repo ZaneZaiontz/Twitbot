ecf13c574c95b0a0dc338aeb5b7d1a42a7dcdf8d
Zane Zaiontz <zanezaiontz@live.com>
2022-07-25 18:46:31 -0500

    Changing things around

--- main.py
+++ main.py
@@ -11,8 +11,48 @@ from csv import reader
 #   -
 '''
 
+
+# Might be better just to handle the data by
+# reading within the __init__ rather than calling
+# and storing in main. 
+class tpyData:
+    # def __init__(self) -> None:
+    API_KEY = ""
+    API_SECRET_KEY = ""
+    BEARER_TOKEN = ""
+        
+    def set_API_KEY(self, API):
+        self.API_KEY = API
+    def set_API_SECRET_KEY(self, SECRET):
+        self.API_SECRET_KEY = SECRET
+    def set_BEARER_TOKEN(self, BEARER):
+        self.BEARER_TOKEN = BEARER
+
+    def get_API_KEY(self):
+        return str(self.API_KEY)
+    def get_API_SECRET_KEY(self):
+        return str(self.API_SECRET_KEY)
+    def get_BEARER_TOKEN(self):
+        return str(self.BEARER_TOKEN)
+
+
+
+
+
 def main():
-    pass
+
+    botMain = tpyData()
+
+    f = open("./login/creds.txt", "r")
+    botMain.set_API_KEY(f.readline().strip())
+    botMain.set_API_SECRET_KEY(f.readline().strip())
+    botMain.set_BEARER_TOKEN(f.readline().strip())
+    f.close()
+
+    print("{}\n{}\n{}".format(botMain.get_API_KEY(), botMain.get_API_SECRET_KEY(), botMain.get_BEARER_TOKEN()))
+    
+
+
 
 if __name__ == "__main__":
     main()
