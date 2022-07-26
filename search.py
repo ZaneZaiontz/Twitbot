4bc520ced71d9b92ba906e18a37e64efc4c07f12
Zane Zaiontz <zanezaiontz@live.com>
2022-07-25 20:39:40 -0500

    Changed main file, added search file

---
+++ search.py
@@ -0,0 +1,9 @@
+import tweepy as tp
+
+def pullTweets(clientInfo):
+    
+    client = tp.Client(clientInfo.get_BEARER_TOKEN)
+    # print("{}\n{}\n{}\n{}\n{}".format(clientInfo.get_API_KEY(), clientInfo.get_API_SECRET_KEY(), clientInfo.get_BEARER_TOKEN(), clientInfo.get_ACCESS_TOKEN(), clientInfo.get_ACCESS_TOKEN_SECRET()))    
+    
+
+    client.search_recent_tweets(query=)
---
+++ tweetBot.py
@@ -0,0 +1,69 @@
+import os, tweepy, search
+from csv import reader
+
+# 
+'''
+# TODO:
+#   Create dev account and setup tweepy
+#   -   Gather data over current trending tweets from the last 24 hours
+#   -   Store data in CSV / prepare
+#   -   
+#   -
+'''
+
+
+# Might be better just to handle the data by
+# reading within the __init__ rather than calling
+# and storing in main. 
+class tpyData:
+    # def __init__(self) -> None:
+    API_KEY = ""
+    API_SECRET_KEY = ""
+    BEARER_TOKEN = ""
+    ACCESS_TOKEN = ""
+    ACCESS_TOKEN_SECRET = ""
+    
+    queryTweets = ""
+
+    def set_API_KEY(self, API):
+        self.API_KEY = API
+    def set_API_SECRET_KEY(self, SECRET):
+        self.API_SECRET_KEY = SECRET
+    def set_BEARER_TOKEN(self, BEARER):
+        self.BEARER_TOKEN = BEARER
+    def set_ACCESS_TOKEN(self, accessTok):
+        self.ACCESS_TOKEN = accessTok
+    def set_ACCESS_TOKEN_SECRET(self, accessTok):
+        self.ACCESS_TOKEN_SECRET = accessTok
+    def set_query_tweets(self, tweets):
+        self.queryTweets = tweets
+
+    def get_API_KEY(self):
+        return self.API_KEY
+    def get_API_SECRET_KEY(self):
+        return self.API_SECRET_KEY
+    def get_BEARER_TOKEN(self):
+        return self.BEARER_TOKEN
+    def get_ACCESS_TOKEN(self):
+        return self.ACCESS_TOKEN
+    def get_ACCESS_TOKEN_SECRET(self):
+        return self.ACCESS_TOKEN_SECRET
+    def get_query_tweets(self):
+        return self.queryTweets
+
+def main():
+    clientInfo = tpyData()
+
+    f = open("./login/creds.txt", "r")
+    clientInfo.set_API_KEY(f.readline().strip())
+    clientInfo.set_API_SECRET_KEY(f.readline().strip())
+    clientInfo.set_BEARER_TOKEN(f.readline().strip())
+    clientInfo.set_ACCESS_TOKEN(f.readline().strip())
+    clientInfo.set_ACCESS_TOKEN_SECRET(f.readline().strip())
+    f.close()
+
+    search.pullTweets(clientInfo)
+    
+
+if __name__ == "__main__":
+    main()
